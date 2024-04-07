d38800bdeb953ab0f4ee0bf9cb4cb14afe4daffb
nileshbiradarr <nileshbiradar2002@gmail.com>
2024-04-07 12:14:12 +0530

    07/04/24
    git commit -m 07/04/24

--- loops/while.py
+++ loops/while.py
@@ -1,11 +1,16 @@
-#even
-i= int(input("enter the number"))
+#printing even number from given number with while loop
+
+i= int(input("enter the number = "))
 
 if  (i%2) == 1:
     h=i%2
     i+=1
+    
+#to find odd number    
 #else:
  #   i+=1
+ 
+ 
 while i <=100:
     print(i)
     i += 2
